64b65c552ede62fa0030b703e4803d6f882843fe
Guilherme Pedro <guilhermepedrov@gmail.com>
2020-11-19 02:55:48 -0300

    Linhas de código propriamente

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,14 +1,23 @@
 package Application;
 
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// Primeira alteração no comentário para ver se está tudo certo
-
 		//Mais uma linha
+		//Outra linha mais
 		
-		//Outro linha a mais
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
 		
+		System.out.println("Bom dia!");
+		System.out.println("Boa tarde!");
+		System.out.println("Boa noite!");
+	
+		sc.close();
+	
 	}
 
 }
